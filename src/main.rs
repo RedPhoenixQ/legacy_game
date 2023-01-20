119c6da81ebe3a5dfb74144ebcce5acd29cc62b5
RedPhoenixQ <steve.dark.03@gmail.com>
2023-01-20 17:06:13 +0100

    Basic browser compatible wasm game

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Resource, Default, Reflect)]
+#[reflect(Resource)]
+struct MoveSpeed(f32);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                // fill the entire browser window
+                fit_canvas_to_parent: true,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(WorldInspectorPlugin)
+        .insert_resource(ClearColor(Color::rgb(0.23, 0.23, 0.23)))
+        .insert_resource(MoveSpeed(0.15))
+        .register_type::<MoveSpeed>()
+        .add_startup_system(setup)
+        .add_startup_system(spawn_player)
+        .add_system(handle_input)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        },
+    ));
+}
+
+fn handle_input(
+    keys: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    move_speed: Res<MoveSpeed>,
+) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
+        direction.y += 1.;
+    }
+    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
+
+    let move_delta = (direction * move_speed.0).extend(0.);
+
+    for mut transform in player_query.iter_mut() {
+        transform.translation += move_delta;
+    }
+}
